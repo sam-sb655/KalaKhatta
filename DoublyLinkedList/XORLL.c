61e1196885e4c8a1542a0e538cf8e9107aa297c6
sam-sb655 <soumyabasuli655@gmail.com>
2025-02-22 02:01:32 +0530

    Update XORLL.c

--- DoublyLinkedList/XORLL.c
+++ DoublyLinkedList/XORLL.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // Node structure
 struct Node {
@@ -12,17 +13,120 @@ struct Node* XOR(struct Node* a, struct Node* b) {
     return (struct Node*)((uintptr_t)(a) ^ (uintptr_t)(b));
 }
 
-// Insert node at the beginning of the XOR Linked List
+// Insert node at the beginning
 void insertAtHead(struct Node** head, int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->npx = *head; // XOR(NULL, current head) = current head
+    newNode->npx = *head; 
 
     if (*head != NULL) {
         (*head)->npx = XOR(newNode, (*head)->npx);
     }
+    *head = newNode;
+}
 
-    *head = newNode; // Move head to the new node
+// Insert node at the end
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->npx = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* curr = *head;
+    struct Node* prev = NULL;
+    struct Node* next;
+
+    while (XOR(prev, curr->npx) != NULL) {
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+
+    curr->npx = XOR(prev, newNode);
+    newNode->npx = curr;
+}
+
+// Delete node from beginning
+void deleteFromHead(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node* temp = *head;
+    struct Node* next = temp->npx;
+
+    if (next != NULL) {
+        next->npx = XOR(temp, next->npx);
+    }
+
+    *head = next;
+    free(temp);
+}
+
+// Delete node from end
+void deleteFromEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node* curr = *head;
+    struct Node* prev = NULL;
+    struct Node* next;
+
+    while (XOR(prev, curr->npx) != NULL) {
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+
+    if (prev != NULL) {
+        prev->npx = XOR(prev->npx, curr);
+    } else {
+        *head = NULL;
+    }
+
+    free(curr);
+}
+
+// Search for an element
+int search(struct Node* head, int key) {
+    struct Node* curr = head;
+    struct Node* prev = NULL;
+    struct Node* next;
+    int pos = 1;
+
+    while (curr != NULL) {
+        if (curr->data == key) {
+            return pos;
+        }
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+        pos++;
+    }
+    return -1;
+}
+
+// Count nodes in the list
+int countNodes(struct Node* head) {
+    struct Node* curr = head;
+    struct Node* prev = NULL;
+    struct Node* next;
+    int count = 0;
+
+    while (curr != NULL) {
+        count++;
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+    return count;
 }
 
 // Print the XOR Linked List in forward direction
@@ -34,26 +138,82 @@ void printList(struct Node* head) {
     printf("XOR Linked List: ");
     while (curr != NULL) {
         printf("%d -> ", curr->data);
-
-        next = XOR(prev, curr->npx); // Compute next node
+        next = XOR(prev, curr->npx);
         prev = curr;
         curr = next;
     }
     printf("NULL\n");
 }
 
+// Print the XOR Linked List in reverse direction
+void printReverse(struct Node* head) {
+    if (head == NULL) return;
+
+    struct Node* curr = head;
+    struct Node* prev = NULL;
+    struct Node* next;
+
+    while (XOR(prev, curr->npx) != NULL) {
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+
+    printf("Reverse: ");
+    while (curr != NULL) {
+        printf("%d -> ", curr->data);
+        next = XOR(prev, curr->npx);
+        prev = curr;
+        curr = next;
+    }
+    printf("NULL\n");
+}
+
+// Free the entire list
+void clearList(struct Node** head) {
+    struct Node* curr = *head;
+    struct Node* prev = NULL;
+    struct Node* next;
+
+    while (curr != NULL) {
+        next = XOR(prev, curr->npx);
+        free(curr);
+        prev = curr;
+        curr = next;
+    }
+    *head = NULL;
+}
+
 // Main function
 int main() {
     struct Node* head = NULL;
 
-    // Inserting nodes at the beginning
     insertAtHead(&head, 10);
     insertAtHead(&head, 20);
     insertAtHead(&head, 30);
     insertAtHead(&head, 40);
-    insertAtHead(&head, 50);
+    insertAtEnd(&head, 5);
 
-    // Printing the XOR Linked List
+    printList(head);
+    printReverse(head);
+
+    printf("Total Nodes: %d\n", countNodes(head));
+
+    int searchKey = 20;
+    int position = search(head, searchKey);
+    if (position != -1) {
+        printf("Element %d found at position %d\n", searchKey, position);
+    } else {
+        printf("Element %d not found in the list.\n", searchKey);
+    }
+
+    deleteFromHead(&head);
+    printList(head);
+
+    deleteFromEnd(&head);
+    printList(head);
+
+    clearList(&head);
     printList(head);
 
     return 0;
